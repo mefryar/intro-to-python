6ad24b12d99ca514d38d23370b605b884619b5f4
mefryar <mfryar.hks@gmail.com>
2017-08-19 16:10:25 -0400

    Personalize inspiring message

--- welcome_script.py
+++ welcome_script.py
@@ -1,7 +1,7 @@
 def welcome_message(name):
 #Prints out a personalised welcome message
-    return "Welcome to this Python script, " + name + "!"
+    return "Keep it up and you will become a ninja, " + name + "!"
 
 #Call the welcome message function with the input "Udacity Student"
 #and print the output
-print(welcome_message("Udacity Student"))
+print(welcome_message("Michael"))
