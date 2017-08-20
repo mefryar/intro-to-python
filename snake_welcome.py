61f092b3d526f2fb5bd236d5e6d2872b0b21f15e
mefryar <mfryar.hks@gmail.com>
2017-08-20 09:48:04 -0400

    Add headers

--- snake_welcome.py
+++ snake_welcome.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+"""Program: Snake Welcome
+Programmer: Michael Fryar
+Created: August 19, 2017
+Purpose: Illustrate basic python script
+"""
+
 how_many_snakes = 1
 snake_string = """
 Welcome to Python3!
--- welcome_script.py
+++ welcome_script.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+"""Program: Welcome Script
+Programmer: Michael Fryar
+Created: August 19, 2017
+Purpose: Illustrate basic python script
+"""
+
 def welcome_message(name):
 #Prints out a personalised welcome message
     return "Keep it up and you will become a ninja, " + name + "!"
